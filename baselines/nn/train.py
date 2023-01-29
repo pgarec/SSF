74a1e0ae7300d0c321673647d3eaecd41112cb72
Pol Garcia Recasens <pgare@theia.imm.dtu.dk>
2023-01-29 17:53:50 +0100

    pipeline working

--- baselines/nn/train.py
+++ baselines/nn/train.py
@@ -1,25 +1,68 @@
-import os 
+import os
 import torch
-import numpy as np 
+import torch.optim as optim
+import numpy as np
 import hydra
 import wandb
 import sys
+from torch.nn import CrossEntropyLoss
+from tqdm import tqdm, trange
 
 sys.path.append("./utils")
+sys.path.append("./models")
 from data_utils import get_mnist_loaders
+from network import Model
 
 
 @hydra.main(config_path="./configurations", config_name="basic.yaml")
 def train(cfg):
     # wandb.init(project="test-project", entity="model-driven-models")
     # wandb.config = cfg
-    data_path = cfg.hyperparameters.data_path
-    digits = cfg.hyperparameters.digits
-    dataset = cfg.hyperparameters.dataset
+    data_path = cfg.data.data_path
+    digits = cfg.data.digits
+    dataset = cfg.data.dataset
+    n_epochs = cfg.hyperparameters.n_epochs
 
-    if dataset == 'MNIST':
+    if dataset == "MNIST":
         train_loader, val_loader, test_loader = get_mnist_loaders(data_path, digits)
+        assert(cfg.data.image_shape == 784)
+    
+    model = Model(cfg)
+    optimizer = optim.Adam(model.parameters(), lr=cfg.hyperparameters.lr, weight_decay=cfg.hyperparameters.w_dc)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=cfg.hyperparameters.step_size, gamma=cfg.hyperparameters.gamma)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    model.cuda()
+    model.train()
 
+    for epoch in trange(epochs):
+        training_loss = 0
+        num_batches = train_loader.n_batches
+        num_data = train_loader.dataset_len
+
+        for batch_idx, (x, y) in enumerate(train_loader):
+            optimizer.zero_grad()
+            out = model(x.to(device))
+            loss = CrossEntropyLoss(out, y)
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+
+            print(f" Training loss:{loss/len(train_loader):.4f}")
+            wandb.log({"Training loss": loss/len(train_loader)})
+
+        with torch.no_grad():
+            loss_val = 0
+            for batch_idx, (x, y) in enumerate(train_loader):
+                out = model(x.to(device))
+                loss = CrossEntropyLoss(out, y)
+                loss_val += loss
+            
+            print(f" Validation loss:{loss/len(train_loader):.4f}")
+            wandb.log({"Validation loss": loss/len(train_loader)})
+
+        torch.save(self.model, "models/model_epoch{}.pth".format(self.epochs))
+    
 
 if __name__ == "__main__":
     train()
--- utils/data_utils.py
+++ utils/data_utils.py
@@ -4,28 +4,32 @@ from torchvision import datasets, transforms
 import torch.utils.data as data_utils
 
 
-def get_mnist_loaders(data_path, digits, batch_size=512, model_class='MLP',
-                      train_batch_size=128, val_size=2000, download=True, device='cpu'):
+def get_mnist_loaders(
+    data_path,
+    digits,
+    batch_size=512,
+    model_class="MLP",
+    train_batch_size=128,
+    val_size=2000,
+    download=False,
+    device="cpu",
+):
     """
     Source: https://github.com/runame/laplace-refinement/blob/308a3ff1f16b69dcb5bcea6ea302cf986e07350b/utils/data_utils.py#L605
     """
-    if 'MLP' in model_class:
+    if "MLP" in model_class:
         tforms = transforms.Compose([transforms.ToTensor(), ReshapeTransform((-1,))])
     else:
         tforms = transforms.ToTensor()
 
-    train_set = datasets.MNIST(data_path, train=True, transform=tforms,
-                               download=download)
-    val_test_set = datasets.MNIST(data_path, train=False, transform=tforms,
-                                  download=download)
+    train_set = datasets.MNIST(data_path, train=True, transform=tforms, download=download)
+    val_test_set = datasets.MNIST(data_path, train=False, transform=tforms, download=download)
 
     Xys = [train_set[i] for i in range(len(train_set))]
-    Xs = torch.stack([e[0] for e in Xys]).to(device)
-    ys = torch.stack([torch.tensor(e[1]) for e in Xys]).to(device)
+    Xs = torch.stack([e[0] for e in Xys]).to(device)  # images
+    ys = torch.stack([torch.tensor(e[1]) for e in Xys]).to(device)  # label
     train_loader = FastTensorDataLoader(Xs, ys, batch_size=train_batch_size, shuffle=True)
-    val_loader, test_loader = val_test_split(val_test_set,
-                                             batch_size=batch_size,
-                                             val_size=val_size)
+    val_loader, test_loader = val_test_split(val_test_set, batch_size=batch_size, val_size=val_size)
 
     return train_loader, val_loader, test_loader
 
@@ -34,12 +38,24 @@ def val_test_split(dataset, val_size=5000, batch_size=512, num_workers=0, pin_me
     # Split into val and test sets
     test_size = len(dataset) - val_size
     dataset_val, dataset_test = data_utils.random_split(
-        dataset, (val_size, test_size), generator=torch.Generator().manual_seed(42)
+        dataset,
+        (val_size, test_size),
+        generator=torch.Generator().manual_seed(42),
     )
-    val_loader = data_utils.DataLoader(dataset_val, batch_size=batch_size, shuffle=False,
-                                       num_workers=num_workers, pin_memory=pin_memory)
-    test_loader = data_utils.DataLoader(dataset_test, batch_size=batch_size, shuffle=False,
-                                        num_workers=num_workers, pin_memory=pin_memory)
+    val_loader = data_utils.DataLoader(
+        dataset_val,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+    )
+    test_loader = data_utils.DataLoader(
+        dataset_test,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+    )
     return val_loader, test_loader
 
 
@@ -52,24 +68,17 @@ class ReshapeTransform:
         return torch.reshape(img, self.new_size)
 
 
-class RotationTransform:
-    """Rotate the given angle."""
-    def __init__(self, angle):
-        self.angle = angle
-
-    def __call__(self, x):
-        return TF.rotate(x, self.angle)
-
-
 class FastTensorDataLoader:
     """
     Source: https://github.com/hcarlens/pytorch-tabular/blob/master/fast_tensor_data_loader.py
     and https://discuss.pytorch.org/t/dataloader-much-slower-than-manual-batching/27014/6
     """
+
     def __init__(self, *tensors, batch_size=32, shuffle=False):
         assert all(t.shape[0] == tensors[0].shape[0] for t in tensors)
         self.tensors = tensors
         self.dataset = tensors[0]
+        self.image_shape = tensors[0].shape[1:]
 
         self.dataset_len = self.tensors[0].shape[0]
         self.batch_size = batch_size
@@ -91,7 +100,7 @@ class FastTensorDataLoader:
     def __next__(self):
         if self.i >= self.dataset_len:
             raise StopIteration
-        batch = tuple(t[self.i:self.i+self.batch_size] for t in self.tensors)
+        batch = tuple(t[self.i : self.i + self.batch_size] for t in self.tensors)
         self.i += self.batch_size
         return batch
 
