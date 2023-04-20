cf19167cb47432fcfb800fcfb47e691a06113ebc
Pol Garcia Recasens <polgarciarecasens@gmail.com>
2023-04-20 16:54:56 +0200

    commit

--- scripts/permutation_job.sh
+++ scripts/permutation_job.sh
@@ -9,6 +9,5 @@
 ###BSUB -Ne 
 #BSUB -o ./lsf_output/modeldriven_s0_b64_a50__%J_%I.out
 #BSUB -e ./lsf_output/modeldriven_s0_b64_a50__%J_%I.err
-
-###source ../../venv/bin/activate
-python permutation_job.sh 
+source ../../venv/bin/activate
+python ./tests/clf_nn_pinwheel.py 
