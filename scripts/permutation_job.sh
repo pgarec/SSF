615b277652642ebecda3b86a79124cd39c3c431d
Pol Garcia Recasens <polgarciarecasens@gmail.com>
2023-04-20 16:50:32 +0200

    changes script cluster

--- scripts/permutation_job.sh
+++ scripts/permutation_job.sh
@@ -10,5 +10,5 @@
 #BSUB -o ./lsf_output/modeldriven_s0_b64_a50__%J_%I.out
 #BSUB -e ./lsf_output/modeldriven_s0_b64_a50__%J_%I.err
 
-source ../../venv/bin/activate
+###source ../../venv/bin/activate
 python permutation_job.sh 
