726e526b99dc456d606a00711c6aa62580612ddc
Pol <s221623@student.dtu.dk>
2023-04-21 09:21:11 +0200

    premerge

--- scripts/permutation_job.sh
+++ scripts/permutation_job.sh
@@ -5,8 +5,9 @@
 #BSUB -n 16
 #BSUB -R "span[hosts=1]"
 #BSUB -M 10000MB
-###BSUB -u polgarciarecasens@gmail.com
-###BSUB -Ne 
+#BSUB -u polgarciarecasens@gmail.com
+#BSUB -B
+#BSUB -N
 #BSUB -o ./lsf_output/modeldriven_%J_%I.out
 #BSUB -e ./lsf_output/modeldriven_%J_%I.err
 source ../../venv/bin/activate
