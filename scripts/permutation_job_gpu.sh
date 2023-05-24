50c81cb99fc1dea999b1f123ff86881127ec2a93
Pol <s221623@student.dtu.dk>
2023-05-24 10:31:06 +0200

    changes gpu script bsub

--- scripts/permutation_job_gpu.sh
+++ scripts/permutation_job_gpu.sh
@@ -4,9 +4,13 @@
 #BSUB -n 1
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 24:00
-#BSUB -R "select[gpu32gb]"
-#BSUB -M 10000MB
+#BSUB -R "rusage[mem=5GB]"
 #BSUB -o ./lsf_output/modeldriven_%J_%I.out
 #BSUB -e ./lsf_output/modeldriven_%J_%I.err
+
 source ../../venv/bin/activate
 python3 ./tests/clf_nn_mnist.py 
+
+
+
+
