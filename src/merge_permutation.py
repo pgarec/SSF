87a0c12880a4517bb41022a47bb489c94781d0ee
Pol Garcia Recasens <polgarciarecasens@gmail.com>
2023-09-05 09:24:04 +0200

    removing optimized version of perm_loss

--- src/merge_permutation.py
+++ src/merge_permutation.py
@@ -18,7 +18,7 @@ from model_merging.permutation import scaling_permutation, l2_permutation
 import numpy as np
 import pickle
 import os
-from multiprocessing import Pool
+import cProfile
 
 
 def logprob_normal(x, mu, precision):
@@ -51,64 +51,6 @@ def evaluate_model(model, val_loader, criterion):
             avg_loss += loss
 
     return avg_loss / len(val_loader)
-
-
-def perm_loss_fisher_optimized(cfg, metamodel, models, grads):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    params = metamodel.get_trainable_parameters()
-    metatheta = nn.utils.parameters_to_vector(params).to(device)
-
-    n_dim = len(metatheta)
-    n_perm = cfg.data.permutations
-    n_models = len(models)
-
-    loss = 0.0
-    m = torch.tensor(cfg.data.m).to(device)
-    cond_prior_prec = cfg.train.weight_decay * torch.ones(m).to(device)
-    
-    for p in range(n_perm):
-        perm = torch.randperm(n_dim, device=device)
-
-        grads_m_list, grads_r_list = [], []
-        theta_m_list, theta_r_list = [], []
-        metatheta_m_list, metatheta_r_list = [], []
-
-        for k in range(n_models):
-            model = models[k].to(device)
-            grad = grads[k]
-            params = model.get_trainable_parameters()
-            theta = nn.utils.parameters_to_vector(params).to(device)
-            grad = nn.utils.parameters_to_vector(grad).to(device)
-
-            theta_r_list.append(theta[perm[m:]])
-            theta_m_list.append(theta[perm[:m]])
-            metatheta_r_list.append(metatheta[perm[m:]])
-            metatheta_m_list.append(metatheta[perm[:m]])
-
-            grads_r_list.append(grad[perm[m:]])
-            grads_m_list.append(grad[perm[:m]])
-
-        grads_r = torch.stack(grads_r_list)
-        grads_m = torch.stack(grads_m_list)
-
-        P_mr = torch.einsum('ij,ik->ijk', grads_m, grads_r).sum(dim=0) / cfg.data.n_examples
-        P_mm = torch.einsum('ij,ik->ijk', grads_m, grads_m).sum(dim=0) / cfg.data.n_examples + torch.diag(cond_prior_prec)
-
-        theta_r = torch.stack(theta_r_list)
-        theta_m = torch.stack(theta_m_list)
-        metatheta_r = torch.stack(metatheta_r_list)
-        metatheta_m = torch.stack(metatheta_m_list)
-
-        m_pred = theta_m.T - torch.linalg.solve(P_mm, P_mr) @ (metatheta_r - theta_r).T
-        p_pred = torch.diagonal(P_mm, dim1=-2, dim2=-1)
-        posterior = logprob_normal_optimized(metatheta_m, m_pred, p_pred).sum()
-
-        cond_prior_m = torch.zeros_like(theta_m[0]).to(device)
-        prior = -(1 - (1 / n_models)) * logprob_normal_optimized(metatheta_m, cond_prior_m, cond_prior_prec).sum()
-
-        loss += (posterior + prior) / (m * n_perm)
-
-    return -loss
 
 
 def perm_loss_fisher(cfg, metamodel, models, grads, fishers):
@@ -165,7 +107,6 @@ def perm_loss_fisher(cfg, metamodel, models, grads, fishers):
 
             # Standard inversion
             m_pred = theta_m - torch.linalg.solve(P_mm, P_mr) @ (metatheta_r - theta_r)
-            # m_pred = theta_m - (torch.diag(1/torch.diagonal(P_mm))) @ P_mr @ (metatheta_r - theta_r)
             p_pred = torch.diagonal(P_mm)
             posterior = logprob_normal(metatheta_m, m_pred, p_pred).sum()
 
@@ -197,7 +138,7 @@ def merging_models_permutation(cfg, metamodel, models, grads, fishers, test_load
             inference_loss.append(inf_loss)
         optimizer.zero_grad()
         
-        l = perm_loss_fisher(cfg, metamodel, models, grads, fishers)
+        l = perm_loss_fisher_optimized(cfg, metamodel, models, grads, fishers)
         l.backward()      
         optimizer.step()
         perm_loss.append(-l.item())
