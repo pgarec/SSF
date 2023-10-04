aacfe79bdba0ac6713f91945997bc2f4f741783e
Pol <Pol>
2023-10-04 14:54:25 -0400

    cleaning code

--- tests/lr_fisher.py
+++ tests/lr_fisher.py
@@ -16,16 +16,26 @@ import math
 import os 
 import pickle
 
-palette = ['#264653', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51']
-meta_color = 'r'
-
-# This makes the figures look like LATEX / comment this if latex not installed
 font = {'family' : 'serif',
-        'size'   : 12}
+        'size'   : 20}
 
 plt.rc('text', usetex=True)
 plt.rc('font', **font)
 plt.rc('text.latex', preamble=r'\usepackage{bm}')
+
+color_palette_1 = ['#335c67','#fff3b0','#e09f3e','#9e2a2b','#540b0e']
+color_palette_2 = ['#177e89','#084c61','#db3a34','#ef8354','#323031']
+color_palette_3 = ['#bce784','#5dd39e','#348aa7','#525274','#513b56']
+color_palette_4 = ['#002642','#840032','#e59500','#e5dada','#02040e']
+color_palette_5 = ['#202c39','#283845','#b8b08d','#f2d449','#f29559']
+
+palette_red = ["#03071e","#370617","#6a040f","#9d0208","#d00000","#dc2f02","#e85d04","#f48c06","#faa307","#ffba08"]
+palette_blue = ["#012a4a","#013a63","#01497c","#014f86","#2a6f97","#2c7da0","#468faf","#61a5c2","#89c2d9","#a9d6e5"]
+palette_green = ['#99e2b4','#88d4ab','#78c6a3','#67b99a','#56ab91','#469d89','#358f80','#248277','#14746f','#036666']
+palette_pink = ["#ea698b","#d55d92","#c05299","#ac46a1","#973aa8","#822faf","#6d23b6","#6411ad","#571089","#47126b"]
+palette_super_red = ["#641220","#6e1423","#85182a","#a11d33","#a71e34","#b21e35","#bd1f36","#c71f37","#da1e37","#e01e37"]
+
+palette = color_palette_1
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--overlapping', '-over', type=bool, default=True)
